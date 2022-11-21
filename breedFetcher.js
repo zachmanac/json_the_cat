2b10d78d0528f58d4f9e180a8e9ba428284dccdb
zachmanac <zachstephenson94@gmail.com>
2022-11-21 13:24:26 -0800

    added mocha/chai assertions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,25 @@
 const request = require('request');
 
-const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (body === "[]") {
+      return callback(`${breedName} breed not found.`);
+    }
 
-  if (body === "[]") {
-    return console.log(`${breed} breed not found.`);
-  }
+    if (error) {
+      return callback(error);
+    }
 
-  if (error) {
-    return console.log(error);
-  }
+    const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
+    if (response) {
+      callback(error, data[0].description);
+    }
 
-  if (response) {
-    console.log(data[0].description);
-  }
+  });
 
-});
+};
+
+module.exports = { fetchBreedDescription };
